cb3924eddbda7d4a045a882ad6cba463f0339a9f
SimoneBuccolieri <simobucc2nd@gmail.com>
2026-02-12 21:52:06 +0100

    chore: fix and image publish

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,7 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
-
 
 const prismaClientSingleton = () => {
-  const adapter = new PrismaBetterSqlite3({
-    url: process.env.DATABASE_URL!.replace('file:', '')
-  })
-  return new PrismaClient({ adapter })
+  return new PrismaClient()
 }
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
